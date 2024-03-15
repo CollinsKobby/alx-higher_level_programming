a32ef0c42664d53259ec45be3a3c6c15beed5ef1
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-15 07:37:40 -0700

    Calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
 from calculator_1 import add, sub, mul, div
+"""if __name__ == "__main__":
+    import sys
+    add(sys.argv[0], sys.argv[0])
+    sub(sys.argv[0], sys.argv[0])
+    mul(sys.argv[0], sys.argv[0])
+    div(int(sys.argv[0]), int(sys.argv[0]))
+    """
+
 a = 10
 b = 5
 
