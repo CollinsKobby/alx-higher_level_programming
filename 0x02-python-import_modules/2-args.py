40272cb357a2b8be42d1607e719d8588924cf057
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-15 08:30:48 -0700

    Args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if len(argv) == 1:
+    print("{} arguments.".format(len(argv) - 1))
+elif len(argv) > 1:
+    if len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
+    elif len(argv) > 2:
+        print("{} arguments:".format(len(argv) - 1))
+    for i in range(1, len(argv)):    
+        print("{}: {}".format(i, argv[i]))
+
+"""if __name__ == "__main__":
+    2-args()"""
