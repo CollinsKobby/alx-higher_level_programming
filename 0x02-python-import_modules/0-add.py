15e142e66989bd67eb4dd2a960d036a55337a560
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-15 07:16:43 -0700

    Add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,9 +2,10 @@
 
 from add_0 import add
 
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     import sys
     add(sys.argv[0], sys.argv[0])
+    """
 a = 1
 b = 2
 print("{} + {} =".format(a, b), add(a, b))
