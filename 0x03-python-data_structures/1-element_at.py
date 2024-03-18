7496704a8179644c9b9dc80d7943e7f431138a53
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-18 03:36:59 -0700

    Element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if 0 <= idx < len(my_list):
+        print("{}".format(my_list[idx]))
+    elif idx < 0:
+        print(None)
+    elif idx > len(my_list):
+        print(None)
