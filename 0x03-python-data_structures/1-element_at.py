99572b75bfe736aac51e9e69772fd130b5aa3591
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-18 04:01:15 -0700

    Element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if 0 <= idx < len(my_list):
-        print("{}".format(my_list[idx]))
+    if idx >= 0 or idx < len(my_list):
+        return print("{}".format(my_list[idx]))
+    elif idx >= len(my_list):
+        return None
     elif idx < 0:
-        print(None)
-    elif idx > len(my_list):
-        print(None)
+        return None
