f0417067991aa3021aac9a11e6924a777fa9b513
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-20 17:55:50 -0700

    Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    upd = list(a_dictionary.keys())
+    for i in upd:
+        if i == key:
+            a_dictionary[key] = value
+        else:
+            a_dictionary[key] = value
+    
+    return a_dictionary
