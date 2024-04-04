ca5da16639afa33c3498df030f34eebf51ae13ae
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-03 20:45:26 -0700

    Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module Documentation
+This a module description of the class Square
+"""
+
+class Square:
+    """Square Class Documentation
+    This a description of the class Square
+    """
+    def __init__(self, size):
+        self.__size = size
