4fe4a04856d69f114b5621c489f7d9c90963d237
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-03 21:52:23 -0700

    Square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,8 +3,14 @@
 THis a module description od a class Square
 """
 
+
 class Square:
+    """Square Class Documentation
+    This a description of the Square class with functions __int__
+    and area and contains exceptions
+    """
     def __init__(self, size=0):
+        """__init__"""
         if (type(size) != int):
             print("size must be an integer")
         if (size < 0):
@@ -13,4 +19,5 @@ class Square:
             self.__size = size
 
     def area(self):
+        """area"""
         return(self.__size * 2)
