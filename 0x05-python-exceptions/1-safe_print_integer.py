3cbbf072a2121b5bedb749d7d4fd04b6b38949ef
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-03 18:26:37 -0700

    Safe print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError as e:
+    except Exception as e:
         return(False)
     else:
         return(True)
