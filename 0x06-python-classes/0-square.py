acb7a76cbc781a840fe5aea9cd72eb63fdc449f4
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-03 20:20:20 -0700

    Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
+"""Module Documantation
+This module defines a simple class called Square
+"""
 class Square:
+    """ Square Class Dcumentation
+    a class representing a Square
+    """
     pass
