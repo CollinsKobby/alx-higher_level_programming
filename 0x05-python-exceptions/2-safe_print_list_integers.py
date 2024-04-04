5bada0958b7f70eb272af51c3a4d30f730014649
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-03 19:02:29 -0700

    Safe print list integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    t = 0
+    for i in range(1, x+1):
+        try:
+            print("{:d}".format(my_list[t]), end="")
+        except Exception:
+            pass
+        else:
+            t += 1
+    print()
+    return(t)
