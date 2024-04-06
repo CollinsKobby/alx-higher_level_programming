cf9736447bf6608193b00c5eaa766b256f19bf94
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-06 09:39:28 -0700

    Square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module Documentation
+A description of a class Square
+"""
+
+
+class Square:
+    """ Class Square Documentation
+    A decription of the class square with methods size and add
+    """
+    def __init__(self, size=0):
+        """__init__"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Print area of the square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """ Print ###"""
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__size):
+            print('#' * self.__size)
