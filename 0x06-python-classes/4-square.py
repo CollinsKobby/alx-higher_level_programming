9fad8b6312ba4ddc78d53413c0bb2fff23ae5867
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-06 09:21:28 -0700

    Square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,15 +10,8 @@ class Square:
     """
     def __init__(self, size=0):
         """__init__"""
-        try:
-            type(size) is int
-            size >= 0
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must bi >= 0")
-        else:
-            self.__size = size
+        
+        self.__size = size
 
     @property
     def size(self):
@@ -26,6 +19,10 @@ class Square:
 
     @size.setter
     def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
