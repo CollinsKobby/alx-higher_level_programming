0cf09ae38ac7f96513b77b8e643dae3d4f4bed40
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-07 20:53:53 -0700

    Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This module takes the first and last name of an individual
+"""
+
+def say_my_name(first_name, last_name=""):
+    """ say_my_nameprovides the first and last name of an individual
+    Args:
+    first_name - first name of the indivdual
+    last_name - lasy name of an individual, set to "" by default
+    return ("My name is <first name> <last name>") """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    else:
+        print ("My name is {} {}".format(first_name, last_name)) 
