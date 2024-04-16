63d0742de9eb5adae05fc63a39ac1273979e09db
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-15 22:44:06 -0700

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" A module description
+A simple class called Rectangle """
+
+
+class BaseGeometry:
+    """ An empty class """
+    def area(self):
+        """ Area of a shape """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """ A simple class called Rectangle that nherits from the
+    superclass BaseGeometry
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        BaseGeometry.__init__(self)
