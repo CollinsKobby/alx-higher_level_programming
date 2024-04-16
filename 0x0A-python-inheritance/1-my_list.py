b6e0cadb5364ac740eb8c21ffc55b2a63c427198
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-15 22:54:23 -0700

    my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,19 +5,6 @@ A module description of a class Called Mylist
 
 
 class MyList(list):
-    def __init__(self):
-        self
-
-    @property
-    def num(self):
-        return self
-
-    @num.setter
-    def num(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Value should be an integer")
-        else:
-            self = value
-
     def print_sorted(self):
-        return sorted(self)
+        self.sort()
+        return self
