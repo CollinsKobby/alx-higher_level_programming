1cd03d3d6dddaae0a70f691706c86b893b3661d3
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-16 18:20:30 -0700

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usrbin/python3
+import json
+""" A module of a function called to_json_string """
+
+
+def to_json_string(my_obj):
+    """ A function called to_json_string that the JSON
+    representation of an object(string) """
+    return json.dumps(my_obj)
