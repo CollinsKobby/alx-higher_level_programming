7f57de7873a78180dd3eab31614c91684239263f
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-22 21:04:52 -0700

    Display #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,23 +95,3 @@ class Rectangle(Base):
         """ used to override to return in a different way """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.__x, self.__y, self.__width, self.__height)
-
-    def update(self, *args, **kwargs):
-        if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.__width = args[1]
-            if len(args) >= 3:
-                self.__height = args[2]
-            if len(args) >= 4:
-                self.__x = args[3]
-            if len(args) >= 5:
-                self.__y = args[4]
-
-        if kwargs:
-            if args is not None:
-                pass
-            if kwargs is not None:
-                for key, value in kwargs.items():
-                    setattr(self, key, value)
