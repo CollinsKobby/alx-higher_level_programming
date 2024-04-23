063c239032a00283de97de789e3ef33a71324553
CollinsKobby <collinsnukporti888@gmail.com>
2024-04-22 21:13:03 -0700

    Update #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,7 +96,6 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.__x, self.__y, self.__width, self.__height)
 
-    
     def update(self, *args, **kwargs):
         """ Update the attributes of the rectangle """
         if args:
