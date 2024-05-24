9b07effa59194189473ab01adbdf795c95dcf271
CollinsKobby <collinsnukporti888@gmail.com>
2024-05-24 08:55:49 -0700

    Create User

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates MYSQL Server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_od_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_od_1'@'localhost' WITH GRANT OPTION
